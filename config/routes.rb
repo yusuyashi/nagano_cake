801f0b21141c7295b76270cb2dd040ffc5e66096
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2023-07-25 16:10:44 +0000

    topとaboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
+  
+  
   devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
 
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+  namespace :public do
+    root to: "homes#top"
+    get '/about', to: "homes#about", as: 'about'
+  end
+  
+  namespace :admin do
+    root to: "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Admin.create!(
-  email: 'admin@gmail.com',
-  password: 'mysecurepassword'
+  email: 'test@gmail.com',
+  password: 'test11'
 )
 
 # This file should contain all the record creation needed to seed the database with its default values.
